484edea6944c35f87ce07a5d3e6d6ac6825b539b
Cristian Monticelli <cristian.monticelli@studenti.isissgobetti.it>
2024-04-09 11:48:32 +0200

    u5eiu

--- esercizio35/monticelli_035.py
+++ esercizio35/monticelli_035.py
@@ -6,5 +6,26 @@ def Welcome():
     return "<p>Welcome to our Weather Application!</p>"
 
 @app.route("/cities")
-def Welcome_to_italy():    
-    return render_template("file.html")
+def Welcome_to_italy(): 
+    messaggio = "yuyu"  
+    return render_template("file.html",messaggio=messaggio)
+
+@app.route("/meteo/")
+@app.route("/meteo/<cities>")
+def meteo(cities=None):
+    return f"Weather for {cities}"
+
+def read_json ():
+    with open("meteo.json", "r") as file_json:
+        try:        
+            mylist = json.load(file_json)    
+        except:        
+            mylist = []
+    
+    return mylist
+
+@app.route("/meteo_dinamico/")
+@app.route("/meteo_dinamico/<citta>")
+def meteo(citta=None):
+    lista_citta = read_json()
+    return f"{lista_citta[citta]}"
