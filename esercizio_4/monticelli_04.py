4e6c0e6ff9e35f3ad6e4ac32f85f32bed48dc975
Cristian Monticelli <cristian.monticelli@studenti.isissgobetti.it>
2025-10-02 13:46:57 +0200

    gjidtyoi57si

--- esercizio_4/monticelli_04.py
+++ esercizio_4/monticelli_04.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 # 2. Connessione: crea il file 'scuola.db' se non esiste
-conn = sqlite3.connect('scuola.db')
+conn = sqlite3.connect('esercizio_4/scuola.db')
 # 3. Creazione Cursore
 cursor = conn.cursor()
 
@@ -28,13 +28,13 @@ try:
 
     
     cursor.executemany(
-        "INSERT INTO Studenti (Matricola, Nome, Cognome) VALUES (?, ?, ?)",
+        "INSERT OR IGNORE INTO Studenti (Matricola, Nome, Cognome) VALUES (?, ?, ?)",
         [(101, 'Mario', 'Rossi'),
         (102, 'Lucia', 'Bianchi'),]
     )
     
     cursor.executemany(
-        "INSERT INTO Esami (Id, Matricola, Corso, Voto) VALUES (?, ?, ?)",
+        "INSERT OR IGNORE INTO Esami (Matricola, Corso, Voto) VALUES (?, ?, ?)",
         [(101,"Matematica",28),
         (101,"Informatica",30),
         (101,"Fisica",27),
@@ -49,10 +49,39 @@ try:
     conn.commit()
 
     # Eseguo una SELECT
-    cursor.execute("SELECT Nome, Cognome FROM Studenti WHERE Matricola = ?", (101,))
-    studente = cursor.fetchone() # I risultati sono tuple
-    if studente:
-        print(f"Studente trovato (SQLite): {studente} {studente}")
+    #1. Elenco di tutti gli studenti (Matricola, Nome, Cognome).
+    cursor.execute("""
+        SELECT *
+        FROM Studenti
+    """)
+    studenti = cursor.fetchall()
+    print("\nElenco di tutti gli studenti (Matricola, Nome, Cognome):")
+    for studente in studenti:
+        print(studente)
+    
+    # 2. Elenco dei corsi e voti sostenuti da uno studente specifico
+    # (usa la matricola 101 come esempio).
+    cursor.execute("""
+        SELECT Matricola, Corso, Voto
+        FROM Esami
+        WHERE Matricola = 101
+    """)
+    studenti = cursor.fetchall()
+    print("\nElenco dei corsi e voti sostenuti da uno studente specifico (usa la matricola 101 come esempio):")
+    for studente in studenti:
+        print(studente)
+        
+    # 3. Numero di esami sostenuti per ciascuno studente (GROUP BY Matricola).
+    cursor.execute("""
+        SELECT S.Nome, S.Cognome, Count(*)
+        FROM Studenti AS S
+        JOIN Esami AS E ON S.Matricola = E.Matricola
+        GROUP BY E.Matricola
+    """)
+    studenti = cursor.fetchall()
+    print("\nNumero di esami sostenuti per ciascuno studente (GROUP BY Matricola):")
+    for studente in studenti:
+        print(studente)
 
 finally:
     # 6. Chiusura Connessione
