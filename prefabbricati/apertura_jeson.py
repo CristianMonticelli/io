be36a4ebebcd3506cd58e42555db6f16a6f08cb2
CristianMonticelli <147493554+CristianMonticelli@users.noreply.github.com>
2025-10-09 10:04:17 +0200

    Delete prefabbricati directory
    mjfhmhg

--- prefabbricati/apertura_jeson.py
+++
@@ -1,9 +0,0 @@
-import json
-Nome = {}
-Nome = []
-FILE_PATH = "Nome.json"  
-try:
-    with open(FILE_PATH, 'r') as file:
-        Nome = json.load(file)
-except FileNotFoundError:
-    pass
--- prefabbricati/controllo_diversta_liste.py
+++
@@ -1,9 +0,0 @@
-N = 6
-prima = [1,3, 1, 2, 1, 1]
-dopo = [1,3, 0, 2, 1, 1]
-t = 0
-for i in range(N):
-    if prima[i]!=dopo[i]:
-        t+=1
-print(t)
-
