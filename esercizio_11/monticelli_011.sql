8c54574259b0650ab652e4f2e435f42ea3e7cc10
Cristian Monticelli <cristian.monticelli@studenti.isissgobetti.it>
2025-11-26 10:27:34 +0100

    esercizio 11

--- esercizio_11/monticelli_011.sql
+++ esercizio_11/monticelli_011.sql
@@ -6,4 +6,16 @@ JOIN Beekeeper as B
 ON A.beekeeper_id = B.id
 JOIN Honey as H
 ON P.honey_id = H.id
-WHERE H.denomination LIKE '%DOP%'
+WHERE H.denomination LIKE '%DOP%';
+
+SELECT region, COUNT(*) as numeroApiari
+FROM Apiary
+GROUP BY region;
+
+SELECT T.denomination, sum(*) as quantitaTipologia
+FROM Production as P
+JOIN Honey as H
+ON P.honey_id = H.id
+JOIN Typologi as T
+ON H.typology_id = T.id
+GROUP BY T.denomination 
